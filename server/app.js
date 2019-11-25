9c6f4da88be2b04592bfc8e66b63a053aadb1292
Michał Worwąg <michalwo@gmail.com>
2019-11-25 19:52:23 +0100

    Add mongoose and create connection to mlab database

--- server/app.js
+++ server/app.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
+const mongoose = require("mongoose");
 
 const app = express();
+
+// connect to mlab database
+mongoose.connect(
+  "mongodb://michal:test1234@ds039211.mlab.com:39211/graphql-michalwo",
+  { useNewUrlParser: true }
+);
+mongoose.connection.once("open", () => {
+  console.log("connected to database");
+});
 
 app.use(
   "/graphql",
